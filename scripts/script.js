3ccbd0c37626b119ee2b74b1da5711c29d3e4da1
Pi-Gul <photon-drive@protonmail.com>
2020-06-18 23:37:46 +0200

    added highlighting

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,7 @@ function changeTheme(event) {
     html.style.setProperty('--main-background', 'hsl(0, 0%, 100%)');
     html.style.setProperty('--stripe-background', 'hsl(225, 100%, 98%)');
     html.style.setProperty('--card-background', 'hsl(227, 47%, 96%)');
+    html.style.setProperty('--card-background-highlight', 'hsl(227, 47%, 91%)');
     html.style.setProperty('--text-two', 'hsl(230, 17%, 14%)');
     html.style.setProperty('--text-one', 'hsl(228, 12%, 44%)');    
   } else {
@@ -21,11 +22,18 @@ function changeTheme(event) {
     html.style.setProperty('--main-background', 'hsl(230, 17%, 14%)');
     html.style.setProperty('--stripe-background', 'hsl(232, 19%, 15%)');
     html.style.setProperty('--card-background', 'hsl(228, 28%, 20%)');
+    html.style.setProperty('--card-background-highlight', 'hsl(228, 28%, 26%)');
     html.style.setProperty('--text-one', 'hsl(228, 34%, 66%)');
     html.style.setProperty('--text-two', 'hsl(0, 0%, 100%)');
   };
   //html.style.backgroundColor = 'red';
   //document.querySelector('html').style.backgroundColor = 'red' 
+};
+function highlightOn(event) {
+  event.target.style.backgroundColor = 'var(--card-background-highlight)';
+};
+function highlightOff(event) {
+  event.target.style.backgroundColor = 'var(--card-background)';
 };
 
 switchBack.addEventListener('click', changeTheme);
@@ -33,5 +41,10 @@ switchBack.addEventListener('mouseover', (event) => {
   switchBack.style.backgroundImage='linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))'
   });
 switchBack.addEventListener('mouseout', (event) => {
-  switchBack.style.backgroundImage='none'
+  switchBack.style.backgroundImage='none';
   });
+
+for (let i = 0; i < cardsArray.length; i++) {
+  cardsArray[i].addEventListener('mouseenter', highlightOn);
+  cardsArray[i].addEventListener('mouseleave', highlightOff);
+};
